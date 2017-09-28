b7f3b8ec5128b7e4c9b75ee2fd3face6288b6a75
Sam Gladstone <gladston@umich.edu>
2017-09-28 11:08:40 -0400

    POSTing Craigslist posts creates them

---
+++ server/controllers/posts.js
@@ -0,0 +1,21 @@
+const Post = require('../models').Post;
+
+module.exports = {
+  create(req, res) {
+    return Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      price: req.body.price,
+      address: req.body.address,
+      bedrooms: req.body.bedrooms,
+      bathrooms: req.body.bathrooms,
+      squareFootage: req.body.squareFootage,
+      parking: req.body.parking,
+      housingType: req.body.housingType,
+      url: req.body.url,
+      craigslistPostId: req.body.craigslistPostId
+    })
+    .then(post => res.status(201).send(post))
+    .catch(error => res.status(400).send(error));
+  }
+};
---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const postsController = require('../controllers').posts;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Posts API!',
+  }));
+
+  app.post('/api/posts', postsController.create);
+};
