5c40d7fbd97fb1b0a0bd89f78b9155a560fe2f46
Sam Gladstone <gladston@umich.edu>
2017-10-13 15:13:11 -0600

    Remove unused dirs/files

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-// const postsController = require('../controllers').posts;
-// const authController = require('../controllers/auth-controller');
-
-// module.exports = (app) => {
-//   app.post('/api/register', authController.register);
-//   app.post('/api/login', authController.login);
-//   app.post('/api/posts', postsController.create);
-//   app.get('/api/posts', postsController.list);
-// };
