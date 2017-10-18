4e48eaddba79449de12b8dabf47004e714e6cc3c
Sam Gladstone <gladston@umich.edu>
2017-10-18 11:41:22 -0600

    Add default group when creating a user; TODO: make undeleteable

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jwt-simple');
-const User = require('../models/user');
 const authConfig = require('../config/auth-config');
+const User = require('../models/user');
+const Group = require('../models/group');
 const Promise = require('bluebird');
 
 router.post('/login', (req, res) => {
@@ -32,6 +33,10 @@ router.post('/register', async (req, res) => {
     password: req.body.password
   };
   const user = await User.forge(userFromRequest).save();
+  // Add user's default group
+  const soloGroup = await Group.forge({ name: 'Just Me' }).save();
+  const retrievedGroup = await Group.where({ id:soloGroup.id }).fetch();
+  retrievedGroup.users().attach(user.id);
   res.json(user.omit('password'));
 });
 
