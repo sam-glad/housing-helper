7fd515380077c3f692f1d66d1733c90280387208
Sam Gladstone <gladston@umich.edu>
2017-10-18 19:37:50 -0600

    Removing last user from group deletes group; permanent group cannot be deleted

--- server/controllers/groups-users-controller.js
+++ server/controllers/groups-users-controller.js
@@ -25,7 +25,36 @@ router.post('/', passport.authenticate('jwt', { session: false }), async (req, r
   }
   catch(error) {
     console.log(error);
-    res.status(400);
+    res.status(400).send();
+  }
+});
+
+router.delete('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const group = new Group();
+    const groupWithUsers = await group.retrieveWithUsers(req.body.group_id);
+    const groupUser = await GroupUser.where({ group_id: req.body.group_id, user_id: req.user.id });
+    let responseBody;
+
+    if (!groupWithUsers.attributes.is_just_me) {
+      await groupUser.destroy();
+      responseBody = { membershipDeleted: true }
+
+      // Destroy group if authenticated user is the only one
+      if (groupWithUsers.related('users').length === 1 && groupWithUsers.related('users').models[0].id === req.user.id) {
+        await groupWithUsers.destroy();
+        responseBody.groupDeleted = true;
+      }
+    } else {
+      // User has tried to remove himself from permanent solo ("Just Me") group
+      return res.status(400).send();
+    }
+
+    return res.status(204).json(responseBody);
+  }
+  catch(error) {
+    console.log(error)
+    res.status(400).send();
   }
 });
 
