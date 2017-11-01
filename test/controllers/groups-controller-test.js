fce2985ad6baf74e9512bac5cb3bad2eed8295db
Sam Gladstone <gladston@umich.edu>
2017-10-31 23:14:38 -0600

    Add tests for /api/groups/:id/users

--- test/controllers/groups-controller-test.js
+++ test/controllers/groups-controller-test.js
@@ -131,9 +131,9 @@ describe('Groups', () => {
       const user = await User.forge(userToSave).save();
       const token = await userHelper.getTokenForUser(user, userToSave.password);
 
-      // WHEN a tokenless request is made to see its posts...
+      // WHEN a request is made to see the group's posts,
+      // but the authenticated user is not in that group...
 
-      // THEN the request should return 401
       // try-catch due to superagent being a presumptious putz:
       // https://github.com/chaijs/chai-http/issues/75
       try {
@@ -141,6 +141,7 @@ describe('Groups', () => {
         .get(`/api/groups/${group.id}/posts`)
         .set('Authorization', `Bearer ${token}`)
       } catch(res) {
+        // THEN the request should return 401
         res.should.have.status(401);
       }
     }); // it should...
@@ -161,7 +162,7 @@ describe('Groups', () => {
       const SecondUserPost = await Post.forge(postHelper.buildPost(secondUser, group, 'Second')).save();
       const firstUserToken = await userHelper.getTokenForUser(firstUser, firstUserToSave.password);
 
-      // WHEN one user makes a GET request to /api/groups/:id/posts
+      // WHEN one user makes a request to get the group's posts
       const res = await chai.request(server)
       .get(`/api/groups/${group.id}/posts`)
       .set('Authorization', `Bearer ${firstUserToken}`)
@@ -172,6 +173,72 @@ describe('Groups', () => {
       res.body.posts.length.should.equal(2);
     }); // it should...
 
+  });
+
+  describe('GET /groups/:id/users', () => {
+    it('should return 401 when no token is provided', async () => {
+      // GIVEN a group...
+      const group = await Group.forge({ name: 'One group' }).save();
+
+      // WHEN a tokenless request is made to see its posts...
+
+      // try-catch due to superagent being a presumptious putz:
+      // https://github.com/chaijs/chai-http/issues/75
+      try {
+        const res = await chai.request(server)
+        .get(`/api/groups/${group.id}/users`)
+      } catch(res) {
+        // THEN the request should return 401
+        res.should.have.status(401);
+      }
+    }); // it should...
+
+    it('should return 401 when a user unconnected to the group is authenticated', async () => {
+      // GIVEN a group...
+      const group = await Group.forge({ name: 'One group' }).save();
+      const userToSave = userHelper.buildUser();
+      const user = await User.forge(userToSave).save();
+      const token = await userHelper.getTokenForUser(user, userToSave.password);
+
+      // WHEN a request is made to see the group's users,
+      // but the authenticated user is not in that group...
+
+      // try-catch due to superagent being a presumptious putz:
+      // https://github.com/chaijs/chai-http/issues/75
+      try {
+        const res = await chai.request(server)
+        .get(`/api/groups/${group.id}/users`)
+        .set('Authorization', `Bearer ${token}`)
+      } catch(res) {
+        // THEN the request should return 401
+        res.should.have.status(401);
+      }
+    }); // it should...
+
+    it('should return the group\'s posts when a user connected to the group is authenticated', async () => {
+      // GIVEN a post belonging to a user belonging to a group
+      // and a post belonging to a different user in that same group
+      const group = await Group.forge({ name: 'One group' }).save();
+      const firstUserToSave = userHelper.buildUser('First');
+      const firstUser = await User.forge(firstUserToSave).save();
+      const secondUserToSave = userHelper.buildUser('Second');
+      const secondUser = await User.forge(secondUserToSave).save();
+
+      await group.users().attach(firstUser);
+      await group.users().attach(secondUser);
+
+      const firstUserToken = await userHelper.getTokenForUser(firstUser, firstUserToSave.password);
+
+      // WHEN one user makes a GET request to /api/groups/:id/posts
+      const res = await chai.request(server)
+      .get(`/api/groups/${group.id}/users`)
+      .set('Authorization', `Bearer ${firstUserToken}`)
+
+      // THEN the response should include both posts belonging to that group
+      res.should.have.status(200);
+      res.body.users.should.be.an('array');
+      res.body.users.length.should.equal(2);
+    }); // it should...
   });
 
 }); // describe 'Groups'
