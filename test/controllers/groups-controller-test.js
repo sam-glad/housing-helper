2a4b01a1d5b43dbf4539f6cea9c541281108dcbf
Sam Gladstone <gladston@umich.edu>
2017-11-08 12:14:36 -0700

    Refactor groups controller test

--- test/controllers/groups-controller-test.js
+++ test/controllers/groups-controller-test.js
@@ -50,62 +50,54 @@ describe('Groups', () => {
 
     it('should return 401 when the user is not a member of the group in question', async () => {
       // GIVEN a user who is a member of one group but is not a member of another...
-      const userToSave = userHelper.buildUser();
-      let user, firstGroup, secondGroup;
-      await bookshelf.transaction(async (trx) => {
-        user = await User.forge(userToSave).save();
-        firstGroup = await Group.forge({ name: 'Connected group' }).save();
-        await firstGroup.users().attach(user, { transacting: trx });
-        secondGroup = await Group.forge({ name: 'Unrelated group' }).save();
-      });
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setupIncludeSecondGroup = true;
+      const includePosts = false;
+      const setup = await fullSetup(setupIncludeSecondGroup, includePosts);
 
       // WHEN that user tries to retrieve info for the group of which he is NOT a member...
       try {
         await chai.request(server)
-        .get(`/api/groups/${secondGroup.id}`)
-        .set('Authorization', `Bearer ${token}`);
+        .get(`/api/groups/${setup.secondGroup.id}`)
+        .set('Authorization', `Bearer ${setup.token}`);
       } catch(res) {
         // THEN the response should have a 401 status code
         res.should.have.status(401);
       }
-    });
-
-    it('should return 400 when passed an invalid group ID', async () => {
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
-
-      try {
-        await chai.request(server)
-        .get(`/api/groups/${badGroupId}`)
-        .set('Authorization', `Bearer ${token}`);
-      } catch(res) {
-        res.should.have.status(400);
-      }
-    });
-
-    it('should retrieve one group when the user is a member of it', async () => {
-      const userToSave = userHelper.buildUser();
-      let user, firstGroup;
-      await bookshelf.transaction(async (trx) => {
-        user = await User.forge(userToSave).save(null, { transacting: trx });  
-        firstGroup = await Group.forge({ name: 'Connected group' }).save(null, { transacting: trx });
-        await firstGroup.users().attach(user, { transacting: trx });
-        const secondGroup = await Group.forge({ name: 'Unrelated group' }).save(null, { transacting: trx });
-      });
-      const token = await userHelper.getTokenForUser(user, userToSave.password);  
-
-      const response = await chai.request(server)
-        .get(`/api/groups/${firstGroup.id}`)
-        .set('Authorization', `Bearer ${token}`);
-
-      response.should.have.status(200);
-      response.body.id.should.equal(firstGroup.id);
-      response.body.name.should.equal(firstGroup.attributes.name);      
     }); // it should...
 
-  }); // describe 'GET /groups/:id
+    it('should return 400 when passed an invalid group ID', async () => {
+      // GIVEN a request with a valid token but invalid group ID URL param
+      const setup = await oneUserOneGroupSetup();
+
+      try {
+        // WHEN the request is made
+        await chai.request(server)
+        .get(`/api/groups/${badGroupId}`)
+        .set('Authorization', `Bearer ${setup.token}`);
+      } catch(res) {
+        // THEN the response should have a 400 status code
+        res.should.have.status(400);
+      }
+    }); // it should...
+
+    it('should retrieve one group when the user is a member of it', async () => {
+      // GIVEN a group and an attached user...
+      const includeGroup = true;
+      const attachUserToGroup = true;
+      const setup = await oneUserOneGroupSetup(includeGroup, attachUserToGroup);
+
+      // WHEN that user tries to view that group
+      const response = await chai.request(server)
+        .get(`/api/groups/${setup.group.id}`)
+        .set('Authorization', `Bearer ${setup.token}`);
+
+      // THEN the request should successfully return the group
+      response.should.have.status(200);
+      response.body.id.should.equal(setup.group.id);
+      response.body.name.should.equal(setup.group.attributes.name);      
+    }); // it should...
+
+  }); // describe GET /groups/:id
 
   describe('GET /api/groups/', () => {
     it('should return 401 when no token is provided', async () => {
@@ -131,31 +123,24 @@ describe('Groups', () => {
 
     it('should retrieve all of an authenticated user\'s groups and no others', async () => {
       // GIVEN two groups and a user as a member of ONE of them...
-      const userToSave = userHelper.buildUser();
-      let user, firstGroup;
-      await bookshelf.transaction(async (trx) => {
-        user = await User.forge(userToSave).save(null, { transacting: trx });;
-        firstGroup = await Group.forge({ name: 'Connected group' }).save(null, { transacting: trx });
-        await firstGroup.users().attach(user, { transacting: trx });
-        const secondGroup = await Group.forge({ name: 'Unrelated group' }).save(null, { transacting: trx });
-      });
-
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const includeSecondGroup = true;
+      const includePosts = false;
+      const setup = await fullSetup(includeSecondGroup, includePosts);
 
       // WHEN that user requests his groups
       const res = await chai.request(server)
         .get('/api/groups/')
-        .set('Authorization', `Bearer ${token}`);
+        .set('Authorization', `Bearer ${setup.firstUserToken}`);
               
       // THEN he should successfully retrieve ONLY his own group
       res.should.have.status(200);
       res.body.should.be.an('array');
       res.body.length.should.equal(1);
-      res.body[0].id.should.equal(firstGroup.id);
-      res.body[0].name.should.equal(firstGroup.attributes.name);
+      res.body[0].id.should.equal(setup.firstGroup.id);
+      res.body[0].name.should.equal(setup.firstGroup.attributes.name);
     }); // it should...
 
-  }); // describe 'GET /groups/'
+  }); // describe GET /groups/
 
   describe('GET /groups/:id/posts', () => {
     it('should return 401 when no token is provided', async () => {
@@ -163,32 +148,25 @@ describe('Groups', () => {
       const group = await Group.forge({ name: 'One group' }).save();
 
       // WHEN a tokenless request is made to see its posts...
-
-      // THEN the request should return 401
       try {
         const res = await chai.request(server)
         .get(`/api/groups/${group.id}/posts`)
       } catch(res) {
+        // THEN the request should return 401
         res.should.have.status(401);
       }
     }); // it should...
 
     it('should return 401 when a user unconnected to the group is authenticated', async () => {
       // GIVEN a group...
-      let group, user;
-      const userToSave = userHelper.buildUser();
-      await bookshelf.transaction(async (trx) => {
-        group = await Group.forge({ name: 'One group' }).save();
-        user = await User.forge(userToSave).save();
-      });
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup(true, false);
 
       // WHEN a request is made to see the group's posts,
       // but the authenticated user is not in that group...
       try {
         const res = await chai.request(server)
-        .get(`/api/groups/${group.id}/posts`)
-        .set('Authorization', `Bearer ${token}`)
+        .get(`/api/groups/${setup.group.id}/posts`)
+        .set('Authorization', `Bearer ${setup.token}`)
       } catch(res) {
         // THEN the request should return 401
         res.should.have.status(401);
@@ -196,14 +174,12 @@ describe('Groups', () => {
     }); // it should...
 
     it('should return 400 when passed an invalid group ID', async () => {
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup();
 
       try {
         await chai.request(server)
         .get(`/api/groups/${badGroupId}/posts`)
-        .set('Authorization', `Bearer ${token}`);
+        .set('Authorization', `Bearer ${setup.token}`);
       } catch(res) {
         res.should.have.status(400);
       }
@@ -212,36 +188,24 @@ describe('Groups', () => {
     it('should return the group\'s posts when a user connected to the group is authenticated', async () => {
       // GIVEN a post belonging to a user belonging to a group
       // and a post belonging to a different user in that same group
-      const firstUserToSave = userHelper.buildUser('First');
-      const secondUserToSave = userHelper.buildUser('Second');
-      let group, firstUser;
-      await bookshelf.transaction(async (trx) => {
-        group = await Group.forge({ name: 'One group' }).save(null, { transacting: trx });
-        firstUser = await User.forge(firstUserToSave).save(null, { transacting: trx });
-        const secondUser = await User.forge(secondUserToSave).save(null, { transacting: trx });
-
-        await group.users().attach(firstUser, { transacting: trx });
-        await group.users().attach(secondUser, { transacting: trx });
-
-        const firstUserPost = await Post.forge(postHelper.buildPost(firstUser.id, group.id, 'First')).save(null, { transacting: trx });
-        const SecondUserPost = await Post.forge(postHelper.buildPost(secondUser.id, group.id, 'Second')).save(null, { transacting: trx });
-        return;
-      });
-
-      const firstUserToken = await userHelper.getTokenForUser(firstUser, firstUserToSave.password);
+      const includeSecondGroup = false;
+      const includePosts = true;
+      const setup = await fullSetup(includeSecondGroup, includePosts);
 
       // WHEN one user makes a request to get the group's posts
       const res = await chai.request(server)
-      .get(`/api/groups/${group.id}/posts`)
-      .set('Authorization', `Bearer ${firstUserToken}`)
+      .get(`/api/groups/${setup.firstGroup.id}/posts`)
+      .set('Authorization', `Bearer ${setup.firstUserToken}`)
 
       // THEN the response should include both posts belonging to that group
       res.should.have.status(200);
       res.body.posts.should.be.an('array');
       res.body.posts.length.should.equal(2);
+      const postIds = res.body.posts.map((post) => { return post.id });
+      postIds.should.have.members([setup.firstUserFirstGroupPost.id, setup.secondUserFirstGroupPost.id]);
     }); // it should...
 
-  });
+  }); // describe GET /groups/:id/posts
 
   describe('GET /groups/:id/users', () => {
     it('should return 401 when no token is provided', async () => {
@@ -260,20 +224,14 @@ describe('Groups', () => {
 
     it('should return 401 when a user unconnected to the group is authenticated', async () => {
       // GIVEN a group...
-      const group = await Group.forge({ name: 'One group' }).save();
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup(true, false);
 
       // WHEN a request is made to see the group's users,
       // but the authenticated user is not in that group...
-
-      // try-catch due to superagent being a presumptious putz:
-      // https://github.com/chaijs/chai-http/issues/75
       try {
         const res = await chai.request(server)
-        .get(`/api/groups/${group.id}/users`)
-        .set('Authorization', `Bearer ${token}`)
+        .get(`/api/groups/${setup.group.id}/users`)
+        .set('Authorization', `Bearer ${setup.token}`)
       } catch(res) {
         // THEN the request should return 401
         res.should.have.status(401);
@@ -281,42 +239,38 @@ describe('Groups', () => {
     }); // it should...
 
     it('should return 400 when passed an invalid group ID', async () => {
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup();
 
       try {
         await chai.request(server)
         .get(`/api/groups/${badGroupId}/users`)
-        .set('Authorization', `Bearer ${token}`);
+        .set('Authorization', `Bearer ${setup.token}`);
       } catch(res) {
         res.should.have.status(400);
       }
     }); // it should...
 
-    it('should return the group\'s posts when a user connected to the group is authenticated', async () => {
+    it('should return the group\'s users when a user connected to the group is authenticated', async () => {
       // GIVEN a post belonging to a user belonging to a group
       // and a post belonging to a different user in that same group
-      const group = await Group.forge({ name: 'One group' }).save();
-      const firstUserToSave = userHelper.buildUser('First');
-      const firstUser = await User.forge(firstUserToSave).save();
-      const secondUserToSave = userHelper.buildUser('Second');
-      const secondUser = await User.forge(secondUserToSave).save();
-
-      await group.users().attach(firstUser);
-      await group.users().attach(secondUser);
-
-      const firstUserToken = await userHelper.getTokenForUser(firstUser, firstUserToSave.password);
+      const includeSecondGroup = false;
+      const includePosts = false;
+      const setup = await fullSetup(includeSecondGroup, includePosts);
 
       // WHEN one user makes a GET request to /api/groups/:id/posts
       const res = await chai.request(server)
-      .get(`/api/groups/${group.id}/users`)
-      .set('Authorization', `Bearer ${firstUserToken}`)
+      .get(`/api/groups/${setup.firstGroup.id}/users`)
+      .set('Authorization', `Bearer ${setup.firstUserToken}`)
 
       // THEN the response should include both posts belonging to that group
+      console.log(JSON.stringify(res.body));
       res.should.have.status(200);
+      res.body.id.should.eql(setup.firstGroup.id);
+      res.body.name.should.eql(setup.firstGroup.attributes.name);
       res.body.users.should.be.an('array');
       res.body.users.length.should.equal(2);
+      const userIds = res.body.users.map((user) => { return user.id; });
+      userIds.should.have.members([setup.firstUser.id, setup.secondUser.id]);
     }); // it should...
   });
 
@@ -338,16 +292,14 @@ describe('Groups', () => {
 
     it('should return 400 with an invalid payload', async () => {
       // GIVEN a user making a request with an invalid payload
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup();
       const emptyPayload = { }; // name is required but not provided
 
       // WHEN the request is made
       try {
         await chai.request(server)
           .post('/api/groups')
-          .set('Authorization', `Bearer ${token}`)
+          .set('Authorization', `Bearer ${setup.token}`)
           .send(emptyPayload);
       } catch(res) {
         // THEN it should get a 400 response
@@ -357,21 +309,78 @@ describe('Groups', () => {
 
     it('should insert a valid group', async () => {
       // GIVEN a user making a request with valid body/headers
-      const userToSave = userHelper.buildUser();
-      const user = await User.forge(userToSave).save();
-      const token = await userHelper.getTokenForUser(user, userToSave.password);
+      const setup = await oneUserOneGroupSetup();
       const groupToInsert = { name: 'This should work' };
 
       // WHEN the request is made with a valid token
       const res = await chai.request(server)
         .post('/api/groups/')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${setup.token}`)
         .send(groupToInsert);
 
       // THEN the request should successfully insert the group
       res.should.have.status(201);
       res.body.name.should.eql(groupToInsert.name);
     }); // it should...
-  }); // describe 'POST /groups'
+  }); // describe POST /groups
 
 }); // describe 'Groups'
+
+async function fullSetup(includeSecondGroup, includePosts) {
+  const firstUserToSave = userHelper.buildUser('First');
+  const secondUserToSave = userHelper.buildUser('Second');
+  const thirdUserToSave = userHelper.buildUser('Third');
+  
+  let firstUser, secondUser, thirdUser, firstGroup, secondGroup, firstUserFirstGroupPost, secondUserFirstGroupPost;
+  await bookshelf.transaction(async (trx) => {
+    firstUser = await User.forge(firstUserToSave).save(null, { transacting: trx });
+    secondUser = await User.forge(secondUserToSave).save(null, { transacting: trx });
+    firstGroup = await Group.forge({ name: 'First group' }).save(null, { transacting: trx });
+    await firstGroup.users().attach(firstUser, { transacting: trx });
+    await firstGroup.users().attach(secondUser, { transacting: trx });
+    
+    if (includePosts) {
+      firstUserFirstGroupPost = await Post.forge(postHelper.buildPost(firstUser.id, firstGroup.id, 'First')).save(null, { transacting: trx });
+      secondUserFirstGroupPost = await Post.forge(postHelper.buildPost(secondUser.id, firstGroup.id, 'Second')).save(null, { transacting: trx });
+    }
+
+    if (includeSecondGroup) {
+      secondGroup = await Group.forge({ name: 'Second Group' }).save(null, { transacting: trx });
+    }
+  });
+  const firstUserToken = await userHelper.getTokenForUser(firstUser, firstUserToSave.password);
+
+  return {
+    firstUser: firstUser,
+    secondUser: secondUser,
+    thirdUser: thirdUser,
+    firstGroup: firstGroup,
+    secondGroup: secondGroup,
+    firstUserFirstGroupPost: firstUserFirstGroupPost,
+    secondUserFirstGroupPost: secondUserFirstGroupPost,
+    firstUserToken: firstUserToken
+  };
+}
+
+async function oneUserOneGroupSetup(includeGroup, attachUserToGroup) {
+  const userToSave = userHelper.buildUser();
+  const groupToSave = { name: 'The only group' };
+  let user, group;
+
+  await bookshelf.transaction(async (trx) => {
+    user = await User.forge(userToSave).save(null, { transacting: trx });
+    if (includeGroup) {
+      group = await Group.forge(groupToSave).save(null, { transacting: trx });
+      if (attachUserToGroup) {
+       await group.users().attach(user, { transacting: trx });
+      }
+    }
+  });
+
+  const token = await userHelper.getTokenForUser(user, userToSave.password);
+  return {
+    user: user,
+    group: group,
+    token: token
+  };
+}
