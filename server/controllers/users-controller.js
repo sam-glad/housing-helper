7d4350e759dcc07b36180cc62c6404981fcbc6b1
Sam Gladstone <gladston@umich.edu>
2017-11-12 20:39:51 -0700

    Apply 500-default changes to users controller

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -3,37 +3,32 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const controllerHelper = require('./controller-helper');
 const knex = require('../../db/knex');
 const Bookshelf = require('../../db/bookshelf');
 
 const User = require('../models/user');
 
-router.get('/search', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    if (!req.query || (!req.query.name && !req.query.emailAddress)) {
-      res.status(400).send('Neither email address nor name provided'); // TODO constants file for this
-    }
+router.get('/search', passport.authenticate('jwt', { session: false }), controllerHelper.wrapAsync(async function(req, res) {
+  if (!req.query || (!req.query.name && !req.query.emailAddress)) {
+    res.status(400).send('Neither email address nor name provided'); // TODO constants file for this
+  }
 
-    let users;
-    if (req.query.emailAddress) {
-      users = await User.where('email_address', req.query.emailAddress).fetchAll();
-    } else if (req.query.name) {
-      const nameForQuery = req.query.name.toLowerCase();
-      users = await User.query((qb) => {
-        qb.select('id', 'name_first', 'name_last', 'name_full')
-          .where(
-            // See "add-lower-name-full-index-to-users" migration,
-            // inspired by: https://stackoverflow.com/questions/1566717/postgresql-like-query-performance-variations
-            knex.raw('LOWER("name_full") LIKE ?', `%${nameForQuery}%`)
-        );
-      }).fetchAll();
-    }
-    res.json(users);
+  let users;
+  if (req.query.emailAddress) {
+    users = await User.where('email_address', req.query.emailAddress).fetchAll();
+  } else if (req.query.name) {
+    const nameForQuery = req.query.name.toLowerCase();
+    users = await User.query((qb) => {
+      qb.select('id', 'name_first', 'name_last', 'name_full')
+        .where(
+          // See "add-lower-name-full-index-to-users" migration,
+          // inspired by: https://stackoverflow.com/questions/1566717/postgresql-like-query-performance-variations
+          knex.raw('LOWER("name_full") LIKE ?', `%${nameForQuery}%`)
+      );
+    }).fetchAll();
   }
-  catch(error) {
-    console.log(error);
-    res.status(400).send();
-  }
-});
+  res.json(users);
+}));
 
 module.exports = router;
